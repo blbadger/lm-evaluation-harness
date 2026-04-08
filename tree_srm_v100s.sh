c577b87721b6f1dd031be4834c531a2dacdc0054
blbadger <blbadger10@gmail.com>
2026-04-08 14:22:11 +0000

    tree specific pass

--- tree_srm_v100s.sh
+++ tree_srm_v100s.sh
@@ -1,4 +1,4 @@
 accelerate launch -m lm_eval --model tree-srm \
     --model_args pretrained=/home/bbadger/Desktop/gsm8k_SFT_srm_c1024/chkpt-300/model.safetensors \
-    --tasks gsm8k_pass@50 \
-    --batch_size 16500
+    --tasks gsm8k_tree_pass \
+    --batch_size 16896
